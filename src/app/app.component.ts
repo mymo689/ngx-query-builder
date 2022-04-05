b3449fad0bae45bf1b625ebf8681bacab6b3bbaa
Myles Morrone <mylesmorrone@gmail.com>
2022-04-05 11:19:41 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-qb-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ngx-query-builder-app';
+}
