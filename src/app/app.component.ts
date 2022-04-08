f9e8e7161e0393d3b8f96eb33e29a0f15d3b7ad4
Myles Morrone <mylesmorrone@gmail.com>
2022-04-08 16:44:16 -0400

    MyMo - setup for local dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Filter, IDataField } from 'ngx-query-builder';
 
 @Component({
   selector: 'ngx-qb-root',
@@ -6,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngx-query-builder-app';
+  filter: Partial<Filter> = {
+    filterLevel: 1,
+    id: 1,
+    isGroupTF: true,
+    clause: 'AND',
+    value: null,
+    value2: null,
+    subFilters: []
+  };
+  dataFieldList: IDataField[] = [
+    {
+      text: 'Author',
+      type: 'string',
+      fieldName: 'author'
+    },
+    {
+      text: 'Title',
+      type: 'string',
+      fieldName: 'title'
+    },
+    {
+      text: 'Date Published',
+      type: 'date',
+      fieldName: 'publishedDT'
+    },
+    {
+      text: 'Copies Sold',
+      type: 'number',
+      fieldName: 'copiesSold'
+    }
+  ]
+
+  filterChanged(x: any): void {
+    console.log('FC', x);
+  }
+
+  filterReset(x: any): void {
+    console.log('FR', x);
+  }
+
+  queryExecuted(x: any): void {
+    console.log('QE', x);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxQueryBuilderModule } from 'ngx-query-builder';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgxQueryBuilderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
